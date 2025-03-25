12893b5d2b30f4711924d956eb83dddc98b22d4d
lordjr05 <lordjr@email.sc.edu>
2025-03-25 09:46:33 -0400

    Class 3/25

---
+++ react-01/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import "./css/Footer.css";
+
+function Footer() {
+    return(
+        <footer>
+            <h1>Footer of the Site</h1>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ react-01/src/pages/Nopage.jsx
@@ -0,0 +1,11 @@
+import './css/Nopage.css';
+
+function Nopage() {
+    return (
+        <>
+            <h1>Sorry, We can't find the page you're looking for.</h1>
+        </>
+    );
+};
+
+export default Nopage;
