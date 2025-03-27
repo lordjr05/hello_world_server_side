0f947b8caabd4687226a7b3966f667cc5347ae4b
lordjr05 <lordjr@email.sc.edu>
2025-03-27 08:42:02 -0400

    Setup Pages

--- react-01/src/index.js
+++ react-01/src/index.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter, Routes,Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './index.css';
 import Layout from './Layout';
 import Home from './pages/Home';
+import Blog from './pages/Blog';
+import About from './pages/About';
+import Nopage from './pages/Nopage';
 
 function App() {
-  return (
-    <BrowserRouter>
+  return(
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path="/" element = {<Layout />}>
-          <Route index element ={<Home />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
           <Route path="about" element={<About />} />
+          <Route path="blog" element={<Blog />} />
+          <Route path="*" element={<Nopage/>} />
         </Route>
       </Routes>
     </BrowserRouter>
   );
 };
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -25,5 +29,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-
